3a4fe28ea749022a8afd71b081596742809103a7
Yusuf Modan <yumodan@gmail.com>
2016-02-05 02:12:56 -0800

    add frontier for bfs_select

--- bfs_graphs.py
+++ bfs_graphs.py
@@ -1,28 +1,33 @@
 import queue
-"""given an adjacency list representation of an undirected graph implement a bfs traversal"""
+"""given an adjacency list representation of an undirected graph implement a bfs traversal
+  bfs_select will return a list of tuples. the 0 index of the tuple will contain the node/vertex
+  traveled to and the 1 index of the tuple will contain the length from the starting node
+"""
 
 def bfs_select(graph, start):
+    result = []
+    frontier = 1
     Q = queue.Queue()
-    visited = visits(graph)
     Q.put(start)
-    visited[start] = True
+    visited = visits(graph)
+    visited[start] = (True, 0)
     while not Q.empty():
         start = Q.get()
         for edge in graph[start]:
-            if not visited[edge]:
+            if not visited[edge][0]:
                 Q.put(edge)
-                visited[edge] = True
-    result = []
+                visited[edge] = (True, frontier)
+        frontier += 1
     for vertex in visited:
-        if visited[vertex]:
-            result.append(vertex)
+        if visited[vertex][0]:
+            result.append((vertex, visited[vertex][1]))
     return result
         
 
 def visits(graph):
     visited = {}
     for vertex in graph:
-        visited[vertex] = False
+        visited[vertex] = (False, 0)
     return visited
 
 
@@ -32,5 +37,5 @@ g = {
   "c": ["d"],
   "d": ["a", "b", "c"]
 }
-print(visits(g))
+# print(visits(g))
 print(bfs_select(g, "b"))
