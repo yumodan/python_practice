2c545503fc48e743c20e123b85884e6f07f3ed4b
Yusuf Modan <yumodan@gmail.com>
2016-02-26 15:08:33 -0800

    fix bfs frontier

--- bfs_graphs.py
+++ bfs_graphs.py
@@ -6,23 +6,23 @@ import queue
 
 def bfs_select(graph, start):
     result = []
-    frontier = 1
     Q = queue.Queue()
-    Q.put(start)
+    Q.put((start, 0))
     visited = visits(graph)
     visited[start] = (True, 0)
     while not Q.empty():
         start = Q.get()
+        frontier = start[1]
+        start = start[0]
         for edge in graph[start]:
             if not visited[edge][0]:
-                Q.put(edge)
-                visited[edge] = (True, frontier)
-        frontier += 1
+                Q.put((edge, frontier + 1))
+                visited[edge] = (True, frontier + 1)
     for vertex in visited:
         if visited[vertex][0]:
             result.append((vertex, visited[vertex][1]))
     return result
-        
+
 
 def visits(graph):
     visited = {}
@@ -30,12 +30,14 @@ def visits(graph):
         visited[vertex] = (False, 0)
     return visited
 
-
+# undirected graph below
 g = {
   "a": ["b", "d"],
   "b": ["d", "a"],
-  "c": ["d"],
-  "d": ["a", "b", "c"]
+  "c": ["d", "e", "f"],
+  "d": ["a", "b", "c"],
+  "e": ["c"],
+  "f": ["c"]
 }
 # print(visits(g))
 print(bfs_select(g, "b"))
